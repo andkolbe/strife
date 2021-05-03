7267a8c28c3f7c2a27985ac4c4000814ac7ea43f
Andrew <kolbe1129@gmail.com>
2021-05-03 15:40:47 -0500

    Removed Redis Adapter

--- public/js/main.js
+++ public/js/main.js
@@ -81,7 +81,7 @@ function outputUsers(users) {
     `;
 }
 
-//Prompt the user before leave chat room
+// Prompt the user before leave chat room
 document.getElementById('leave-btn').addEventListener('click', () => {
     const leaveRoom = confirm('Are you sure you want to leave the chatroom?');
     if (leaveRoom) {
